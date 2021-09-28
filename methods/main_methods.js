6c98bd53da30347e73ad50b815daa24170416df3
luis.sanchez <luis.sa1973@gmail.com>
2021-09-28 09:16:03 +0200

    adding methods

---
+++ methods/main_methods.js
@@ -0,0 +1,29 @@
+
+
+//? scriptLog(logLevel, message);
+
+//? jobLog(jobName,jobGroupName,"SUCCESS","message");
+
+//* var outputParams=new java.util.HashMap();
+
+//* outpuParams.put("returnType","SUCCES");
+
+//* outpuParams.put("returnMessage","message");
+
+//* outpuParams.put("returnType","WARNING");
+
+//* outpuParams.put("returnMessage","message");
+
+//* outpuParams.put("returnType","ERROR");
+
+//* outpuParams.put("returnMessage","message");
+
+//? ibappsSetSuccessMessage("ok","ok");
+
+//? ibappsSetWarningMessage("advise","advise");
+
+//? ibappsSetSuccessMessage(codeError,"error","error");
+
+//? var it = inputParams.keySet().toArray(); forEach 
+
+//? var it = inputParams.keySet().iterator(); while(it.hasNext()) {it.next()}
