cce106235ff9d91d8e9787ecf315a3fea9f24756
luis.sanchez <luis.sa1973@gmail.com>
2021-09-28 10:10:08 +0200

    adding methods java

---
+++ java/methods.js
@@ -0,0 +1,25 @@
+
+
+//* Object.setKeys() return an hashMap of object members.
+//* Object.values() return an hashMap of object values.
+
+//* Object.setKeys().toArray() return an array of object members.
+//* Object.values().toArray() return an array of object values.
+//? After that the returned value can use forEach loop function(value,index,array)
+
+//* Object.setKeys().iterator() return an array of object members.
+//* Object.values().iterator() return an array of object values.
+//? After that you can use:
+
+//* var it=Object.setKeys();
+//* it.size(); number of elements for(var i=0;i.size();i++)
+
+//* var it=Object.setKeys().iterator();
+
+//* while(it.hasNext()) {
+//*     var key=String(it.next());
+//*     console.log(Object.get(key))
+
+//*  scriptLog("DEBUG",Object.get(key))
+//* }
+
